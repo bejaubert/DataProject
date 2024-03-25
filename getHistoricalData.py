e852208f16aa44c4eb4adf9d7648c266c7a6358f
Exeio <andrirazafy9@gmail.com>
2024-03-25 21:04:28 +0100

    correction bug affichage date + total retrieve

--- getHistoricalData.py
+++ getHistoricalData.py
@@ -1,31 +1,30 @@
 import yfinance as yf
-import pandas as pd
 from datetime import datetime
-import openpyxl 
+import pandas as pd
 
-class CAC40HistoricalData:
-    def __init__(self, ticker_symbol="^FCHI"):
+class StockHistoricalData:
+    def __init__(self, ticker_symbol="TTE.PA"):  # symbole boursier de Total ou peu importe
         self.ticker_symbol = ticker_symbol
 
     def fetch_data(self):
-        # Crée un objet Ticker pour le CAC 40
-        cac40 = yf.Ticker(self.ticker_symbol)
+        # Crée un objet Ticker pour l'action spécifiée
+        stock = yf.Ticker(self.ticker_symbol)
         # Récupère toutes les données historiques disponibles
-        data = cac40.history(period="max")
-        # Assurez-vous que l'index est sans timezone pour éviter les problèmes lors de l'exportation vers Excel
+        data = stock.history(period="max")
+        # S'assurer que l'index est sans timezone pour éviter les problèmes lors de l'exportation vers Excel
         data.index = data.index.tz_localize(None)
         return data
 
-    def save_to_excel(self, filename="CAC40_Historical_Data.xlsx"):
+    def save_to_excel(self, filename="Total_Historical_Data.xlsx"):  # Modifier ici le nom du fichier par défaut
         data = self.fetch_data()
-        # Obtient la date actuelle
+        # Formattez les dates dans un format lisible ('YYYY-MM-DD') avant de sauvegarder
+        data.index = data.index.strftime("%Y-%m-%d") # corrige le problème de date en visualisant l'excel
         current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # Ajoute une colonne 'date_modification' avec la date actuelle pour chaque ligne
         data['date_modification'] = current_date
         data.to_excel(filename)
-        print(f"Les données historiques du CAC 40 ont été sauvegardées dans {filename}, avec la date de modification.")
+        print(f"Les données historiques de l'action Total ont été sauvegardées dans {filename}, avec la date de modification.")
 
 # Utilisation de la classe
 if __name__ == "__main__":
-    cac40_data = CAC40HistoricalData()
-    cac40_data.save_to_excel()
+    stock_data = StockHistoricalData()
+    stock_data.save_to_excel()
